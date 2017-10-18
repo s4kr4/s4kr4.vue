a4411ac4f5864b07048bad3edbed14a623f18e7c
s4kr4 <s4kr4m4@gmail.com>
2017-10-18 23:56:46 +0900

    Initial commit

---
+++ src/components/Menu.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="menu">
+    <ul>
+      <li><a href="#">GitHub</a></li>
+      <li><a href="#">Qiita</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'menu'
+}
+</script>
+
+<style scoped>
+li {
+  list-style: none;
+  font-size: 1.5em;
+}
+
+a {
+  color: #a00;
+  text-decoration: none;
+}
+</style>
